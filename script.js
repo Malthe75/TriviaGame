9a637f81052fff1768b3243ca4371dd1164403cd
Malthe75 <Malthehf@gmail.com>
2025-02-27 19:52:57 +0100

    small logic

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+function answerClicked(){
+    console.log("This is tesxt?");
+    document.getElementById("question").innerHTML = "NEXT QUESTION";
+    
+    for (let index = 1; index < 5; index++) {
+        document.getElementById("answer" + index).innerHTML = "NextAnswer" + index;
+        
+    }
+}
